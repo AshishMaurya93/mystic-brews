9c689feaf4e8ea87ba65fe972c63e64d64183ce8
Ashish Maurya <ashishmaurya290@gmail.com>
2025-04-04 13:04:23 +0530

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '🌱Mystic Brews🧪',
+  description: 'Created by Ashish',
+  keywords: 'Potion, Game, Mystic, Brews',
+  generator: 'Ashish',
 }
 
 export default function RootLayout({
